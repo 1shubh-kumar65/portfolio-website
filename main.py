9e22be0c4317346d3cfdb36daa6300036d0b338f
1shubh-kumar65 <162529083+1shubh-kumar65@users.noreply.github.com>
2024-08-01 18:35:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,76 +1,26 @@
-import os
-import google.generativeai as genai
 import streamlit as st
+import webbrowser
 
-# api_key='AIzaSyBGSvpA5Ex9TOBBZSe2tiNzHYe-fAJ37O8'
+import inputs.voice_input as voice_input
+import inputs.camera_input as camera_input
+import inputs.text_input as text_input 
+from main_data import popular_websites
 
-genai.configure(api_key='AIzaSyBGSvpA5Ex9TOBBZSe2tiNzHYe-fAJ37O8')
+# Create a simple navigation bar
+st.sidebar.title("Navigation")
+option = st.sidebar.radio("Go to", ["voice input", "camera input", "text input"])
 
-model = genai.GenerativeModel('gemini-1.5-flash')
+# voice input
+if option == "voice input":
+    voice_input.start()
 
-col1, col2 = st.columns(2)
+# camera input
+elif option == "camera input":
+    camera_input.camera_input()
 
-with col1:
-    st.subheader("HI :wave:")
-    st.title('I am Shubh Kumar')
-
-with col2:
-    st.title('place an image here')
-
-persauna_about_me = '''
-you are shubhs ai bot and will answer any questions related to shubh based on below data.
-you will answer only in first person and not in second and third person.
-the data is :
-my name is subh kumar i am from delhi india currently studying in grade 8th .
-my hobbies:
-programing - i am learning python for 2 yrs through youtube an purchased a udamy cource this would my first python contest
-cooking - i am pretty good at cooking can can make any dish if i know the recipy of it
-skating - i am learning to skate since grade 1 and have also participated in some competions
-fool ball, volly ball - i am learning foot ball and volly ball since lockdown in india 
-'''
-
-st.title(' ')
-st.title('MY AI BOT')
-user_question = st.text_input('Ask anything about me')
-response = model.generate_content(persauna_about_me + 'user input is:' + user_question)
-
-try:
-    # my ai code writer
-
-    persauna = '''
-    you are an personal ai bot you only have to generate python code to execute
-    the task given by the user(generate only code and nonething 
-    else not even the explanation because the code will be copied
-    to another file to be excuted be exucted) and also make sure there is alweys a way to stop the code
-    and make sure there is always gui in it if it is a game make sure it is playable and intresting
-    '''
-
-    user_input = st.text_input('enter a prompt')
-
-    st.write(user_input)
-
-
-    response = model.generate_content(persauna + 'user input is:' + user_input)
-
-    code = response.text.replace("```python", '')
-    code = code.replace("```", '')
-
-    
-    st.write(code)
-
-
-    if st.button('generate'):  
-        with open('test.py', 'r') as a:
-            st.writ(a.readlines(code))
-
-        # Specify the path to your Python file (replace 'path/to/your/script.py' with the actual path)
-        script_path = 'test.py'
-
-        # Check if the file exists
-        if os.path.isfile(script_path):
-            # Execute the Python script using os.system()
-            os.system(f'python {script_path}')
-        else:
-            print(f"Error: The file '{script_path}' does not exist.")
-except Exception as e:
-    st.write(e)
+# text input
+elif option == "text input":
+    command = text_input.text_input()
+    if st.button('generate'):
+        
+        
